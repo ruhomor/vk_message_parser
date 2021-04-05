c6a002ba624de3735336c82f83ad3bfe760bc37d
ruhomor <ruhomor@icloud.com>
2021-04-05 04:17:16 +0300

    Implemented login function

--- vk/settings.py
+++ vk/settings.py
@@ -14,10 +14,10 @@ NEWSPIDER_MODULE = 'vk.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'vk (+http://www.yourdomain.com)'
+USER_AGENT = 'vk (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -25,7 +25,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
--- vk/spiders/vk_spider.py
+++ vk/spiders/vk_spider.py
@@ -5,10 +5,41 @@ import json
 from vk import pipelines
 
 
-class VkSpiderSpider(scrapy.Spider):
+class VkSpiderSpider(Spider):
     name = 'vk_spider'
     allowed_domains = ['vk.com']
-    start_urls = ['http://vk.com/']
+    start_urls = ['http://vk.com/login']
 
-    def parse(self, response):
+    def parse(self, response):  # login function
+        email = input()
+        password = input()
+        ip_h = response.xpath('//*[@id="login_form"]/*[@name="ip_h"]/@value').extract_first()
+        lg_h = response.xpath('//*[@id="login_form"]/*[@name="lg_h"]/@value').extract_first()
+        self.logger.log("got ip_h: {}".format(ip_h))
+        self.logger.log("got lg_h: {}".format(ip_h))
+        yield FormRequest.from_response(response,
+                                        formdata={"act": "login",
+                                                  "role": "al_frame",
+                                                  "expire": "",
+                                                  "to": "bG9naW4-",  # bG9naW4-
+                                                  "recaptcha": "",
+                                                  "captcha_sid": "",
+                                                  "captcha_key": "",
+                                                  "_origin": "https://vk.com",
+                                                  "ip_h": ip_h,
+                                                  "lg_h": lg_h,
+                                                  "ul": "",
+                                                  "email": email,
+                                                  "pass": password},
+                                        callback=self.after_login)
         pass
+
+    def after_login(self, response):
+        if "Error while logging in" in response.body:
+            self.logger.error("Login failed!")
+        else:
+            self.logger.error("Login succeeded!")
+            item = VkMessage()
+            item["quote"] = response.css(".text").extract()
+            item["author"] = response.css(".author").extract()
+            return item
